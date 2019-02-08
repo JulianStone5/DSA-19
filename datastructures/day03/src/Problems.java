8ed738f222ea7d03f0afc5c8f77516ff8204c645
JulianStone5 <julian.stone@students.olin.edu>
2019-02-07 21:30:46 -0500

    Finished Day03 Assignment

--- datastructures/day03/src/Problems.java
+++ datastructures/day03/src/Problems.java
@@ -13,21 +13,68 @@ public class Problems {
     }
 
     public static List<Integer> removeKDigits(int[] A, int k) {
-        // TODO: your code here
-        // For now, return a List that's correct size, but contains only 0s
-        List<Integer> l = new LinkedList<>();
-        for (int i = 0; i < A.length - k; i++) l.add(0);
+        LinkedList<Integer> l = new LinkedList<>();
+        int x = 0;
+        for(int i = 0; i < A.length; i++) {
+            while (!l.isEmpty() && A[i] < l.getLast() && x < k) {
+                l.removeLast();
+                x++;
+            }
+            if (l.size() < A.length - k) {
+                l.addLast(A[i]);
+            }
+        }
         return l;
     }
 
     public static boolean isPalindrome(Node n) {
-        // TODO: your code here
-        return false;
+        if(n == null) { return true; }
+        int size = 1;
+        Node current = n;
+        while(current.next != null) {
+            size++;
+            current = current.next;
+        }
+        if(size == 1) { return true; }
+        int count = 1;
+        current = n;
+        Node prev = null;
+        Node nxt = null;
+        while(count < size/2) {
+            nxt = current.next;
+            current.next = prev;
+            prev = current;
+            current = nxt;
+            count++;
+        }
+        Node head1 = current;
+        Node head2 = current.next;
+        head1.next = prev;
+        if(size % 2 == 1) { head2 = head2.next; }
+        current = head1;
+        Node current2 = head2;
+        while(current.next != null && current2.next != null) {
+            if(current.val != current2.val) { return false; }
+            current = current.next;
+            current2 = current2.next;
+        }
+        return true;
     }
 
     public static String infixToPostfix(String s) {
-        // TODO
-        return null;
+        String str = s.replaceAll(" ","");
+        String operation = "";
+        LinkedList<String> operands = new LinkedList<>();
+        for(int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))) {
+                operation += str.charAt(i) + " ";
+            } else if(str.charAt(i) == ')') {
+                operation += operands.pop() + " ";
+            } else if(str.charAt(i) != '('){
+                operands.push("" + str.charAt(i));
+            }
+        }
+        return operation.substring(0,operation.length()-1);
     }
 
 }
