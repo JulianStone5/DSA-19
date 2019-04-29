aeafa2820905dec27ccf061e373fea928e9378d0
Julian Stone <julian.stone@students.olin.edu>
2019-04-29 16:27:58 -0400

    Finished first DP assignment

--- DP/dp00/src/LongestIncreasingSubsequence.java
+++ DP/dp00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,33 @@
+import java.util.HashMap;
+
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: O(n^2)
+    // Space: O(n)
     public static int LIS(int[] A) {
-        // TODO
-        return 0;
+        if(A.length == 0) {return 0;}
+        int[] DP = new int[A.length];
+        lts(0,A,DP);
+        int maxLen = 0;
+        for(int i = 0; i < DP.length; i++) {
+            maxLen = (maxLen > DP[i]) ? maxLen : DP[i];
+        }
+        return maxLen;
+    }
+
+    public static int lts(int i, int[] A, int[] DP) {
+        if(DP[i] != 0) { return DP[i]; }
+        if(i == A.length - 1) { DP[i] = 1;}
+        else {
+            int maxLen = 0;
+            for (int a = i + 1; a < A.length; a++) {
+                int len = lts(a, A, DP);
+                if (A[a] > A[i]) {
+                    maxLen = (len > maxLen) ? len : maxLen;
+                }
+            }
+            DP[i] = 1 + maxLen;
+        }
+        return DP[i];
     }
 }
